302626c6de978777478d7f1c892cc01ec918fb5e
James Thomas Davey <jamesthomasdavey@Jamess-MacBook-Pro.local>
2018-04-24 11:21:09 -0700

    fix rotation bugs

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,7 +3,16 @@ const diceImageEl = document.querySelector(`.dice__image`);
 
 diceTriggerEl.addEventListener(`click`, rollDice);
 
+randomNum = () => {
+  return Math.floor(Math.random() * 6) + 1
+};
+
 function rollDice() {
-  let randomNum = Math.floor(Math.random() * 6) + 1;
-  diceImageEl.src = `assets/images/dice${randomNum}.png`;
+  diceImageEl.classList.add(`rotate`);
+  setTimeout(function () {
+    diceImageEl.classList.remove(`rotate`)
+  }, 300);
+  setTimeout(function () {
+    diceImageEl.src = `assets/images/dice${randomNum()}.png`
+  }, 300);
 }
